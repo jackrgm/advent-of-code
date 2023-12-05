e97f6ac50d23ad7a2157fbd6c23049180baf1afb
Jack Morgan <jmorgan.dev@pm.me>
2023-12-05 14:52:45 +0000

    Implement 2023 day 1, part 1 in C

--- c/2023/day1.c
+++ c/2023/day1.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-int aoc2023_day1_part1() {
-	printf("[AOC2023-D1-P1] WARNING: Not yet implemented. Returning default '0'.\n");
+int aoc2023_day1_part1(FILE *file) {
+	int ch;
+	int concat;
+	int total = 0;
 
-	return 0;
+	bool is_first = true;
+	bool second_set = false;
+
+	int first = 0;
+	int second = 0;
+
+	while ((ch = getc(file)) != EOF) {
+        if (isdigit(ch)) {
+			if (is_first) {
+					is_first = false;
+					first = ch - '0';
+			} else {
+				second = ch - '0';
+				second_set = true;
+			}
+        } else if (ch == '\n') {
+			// If a second integer isn't found, use the first for concatenation.
+			if (!second_set) {
+				second = first;
+			}
+
+			concat = first * 10 + second;
+			total += concat;
+
+			is_first = true;
+			second_set = false;
+
+			concat = 0;
+			first = 0;
+			second = 0;
+        }
+    }
+
+    concat = first * 10 + second;
+    total += concat;
+
+	return total;
 }
--- c/aoc.c
+++ c/aoc.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-#include "app.h"
 #include "2023/day1.h"
 
 void run_all() {
-	printf("2023 DAY 1 PART 1: %d\n", aoc2023_day1_part1());
+	printf("Not yet implemented.\n");
 }
 
 void run_solution(int year, int day, int part) {
@@ -12,8 +11,20 @@ void run_solution(int year, int day, int part) {
 }
 
 int main(int argc, char **argv) {
-	// TODO Set up run_all() as a console option.
-	run_all();
+	// TODO Parse arguements (-a/--all, -d2021.25.2, etc.)
+	// TODO Supply file path as arg
+
+	FILE *file;
+	file = fopen("inputs/2023/day1.txt", "r");
+
+	if (file == NULL) {
+		printf("%s: Error opening the file.\n", argv[0]);
+		return 1;
+	}
+
+	printf("2023 DAY 1 PART 1: %d\n", aoc2023_day1_part1(file));
+
+	fclose(file);
 
 	return 0;
 }
