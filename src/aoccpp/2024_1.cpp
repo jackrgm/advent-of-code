5d31c82530f9b86c9b24a571ec9ed1bdfe74d4e9
Jack Morgan <jmorgan.dev@pm.me>
2024-12-18 14:53:01 +0000

    WIP: added 2024d1cpp solution structure

--- src/aoccpp/2024_1.cpp
+++ src/aoccpp/2024_1.cpp
@@ -2,9 +2,29 @@
 #include "aoccpp/2024.h"
 
 namespace AOCCPP {
-	int AOC2024::day1(std::ifstream& file, int part) {
+	static void getColumns(std::ifstream& file, std::vector<int>& leftcol,
+		std::vector<int>& rightcol) {
+		// TODO Implement: read file and capture columns accurately
+	}
+
+	static int getTotalDiff(const std::vector<int>& leftcol,
+		const std::vector<int>& rightcol) {
 		// TODO Implement
-		std::cout << "aoc2024day1 WIP\n";
-		return -1;
+			// For each index, calculate the difference between the two columns
+			// for that equivalent index (leftcol[i], rightcol[i]), and add it
+			// to a running total (result += differenceForThisIndex).
+			// At the end, return result.
+	}
+
+	int AOC2024::day1(std::ifstream& file, int part) {
+		std::vector leftcol;
+		std::vector rightcol;
+
+		getColumns(file, leftcol, rightcol);
+		
+		std::sort(leftcol.begin(), leftcol.end());
+		std::sort(rightcol.begin(), rightcol.end());
+
+		return getTotalDiff(leftcol, rightcol);
 	}
 }
